d62a4a735000f4bae7b2f73727292129c39f1646
Darshan Parajuli <parajulidarshan@gmail.com>
2015-10-12 19:59:36 -0700

    works!

--- connect_four_local.py
+++ connect_four_local.py
@@ -6,19 +6,34 @@ def main() -> None:
     pass
 
 
+def print_instructions() -> None:
+    print("Type in column number to drop your piece")
+    print("For example, to drop a piece at sixth column, type '6'")
+    print("Type in column number prefixed by letter p to pop the bottom piece")
+    print("For example, 'p6' will pop the bottom piece at sixth column")
+    print()
+
 def test() -> None:
+    print_instructions()
+    
     game_state = connectfour.new_game()
     utils.print_board(game_state[utils.BOARD])
 
     winner = connectfour.NONE
     players = ("RED", "YELLOW")
     i = 0
+    
     while True:
         player_move = utils.get_input(players[i])
-        if player_move["pop"]:
-            game_state = connectfour.pop(game_state,player_move["col"])
-        else:
-            game_state = connectfour.drop(game_state, player_move["col"])
+        try:
+            if player_move["pop"]:
+                game_state = connectfour.pop(game_state,player_move["col"])
+            else:
+                game_state = connectfour.drop(game_state, player_move["col"])
+        except connectfour.InvalidMoveError:
+            print("invalid move")
+            continue
+
         utils.print_board(game_state[utils.BOARD])
         i = (i + 1) % 2
 
