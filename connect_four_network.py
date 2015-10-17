f9f994e22399009a91d0e4071e76dfac79c4510f
AnthonyLam <thelamacmdr@gmail.com>
2015-10-16 18:17:25 -0700

    Initial network code

--- connect_four_network.py
+++ connect_four_network.py
@@ -1,5 +1,21 @@
+import connect_four_network_utils as net_utils
+import connect_four_utils as utils
+
 def main() -> None:
-    pass
+    is_connected = False
+    connection = net_utils.connect_to_server()
+    user = net_utils.get_username()
+    is_connected = net_utils.start_game(connection,user)
+    print("Game Started")
+    utils.print_instructions()
+
+    while is_connected:
+        #Play our game
+        utils.print_board()
+
+
+
+
 
 if __name__ == "__main__":
     main()
